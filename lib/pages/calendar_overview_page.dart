356e97f85fbdccf8ea03e132a805f0c602178192
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-12-18 19:43:28 +0000

    adds calendar initial state

--- lib/pages/calendar_overview_page.dart
+++ lib/pages/calendar_overview_page.dart
@@ -1,5 +1,4 @@
 import 'package:device_calendar_sandbox/cubit/add_to_calendar_cubit.dart';
-import 'package:device_calendar_sandbox/widgets/event_form_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/add_to_calendar_cubit.dart';
@@ -14,6 +13,8 @@ class CalendarOverviewPage extends StatefulWidget {
 
 class _CalendarOverviewPageState extends State<CalendarOverviewPage> {
   var _calendarEvent;
+  var _calendars;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,76 +23,76 @@ class _CalendarOverviewPageState extends State<CalendarOverviewPage> {
       ),
       body: BlocConsumer<AddToCalendarCubit, AddToCalendarState>(
         listener: (context, state) {
-          if(state is AddToCalendarLoadCalendarsInProgress){
+          if (state is AddToCalendarLoadCalendarsInProgress) {
             return Center(
               child: CircularProgressIndicator(),
             );
-          } else if(state is AddToCalendarLoadCalendarsFailure){
+          } else if (state is AddToCalendarLoadCalendarsFailure) {
             Scaffold.of(context).showSnackBar(SnackBar(
               content: Text(state.message),
             ));
-          } else if (state is AddToCalendarLoadCalendarsSuccess){
+          } else if (state is AddToCalendarLoadCalendarsSuccess) {
             Scaffold.of(context).showSnackBar(SnackBar(
               content: Text(state.message),
             ));
-          }
-        },
-        builder: (context, state){
-          if(state is AddToCalendarInitial) {
-            return _buildInitialLayout();
-          } else {
-            return CircularProgressIndicator();
-          }
-        },
-      ),
-      floatingActionButton: BlocConsumer<AddToCalendarCubit, AddToCalendarState>(
-        listener: (context, state) {
-          if(state is AddToCalendarFailure) {
-            Scaffold.of(context).showSnackBar(
-              SnackBar(content: Text(state.message),
-              )
-            );
-          }
-          if(state is AddToCalendarSuccess) {
-            Scaffold.of(context).showSnackBar(
-              SnackBar(content: Text(state.message),
-              )
-            );
           }
         },
         builder: (context, state) {
-          if(state is AddToCalendarInitial) {
-            return FloatingActionButton(
-              onPressed: () {
-                final calendarCubit = context.bloc<AddToCalendarCubit>();
-                calendarCubit.addToCalendar(_calendarEvent, '2');
-              },
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            );
-          } else if (state is AddToCalendarInProgress) {
-            return CircularProgressIndicator();
-          } else if (state is AddToCalendarSuccess) {
-            return FloatingActionButton(
-              onPressed: () {
-                final calendarCubit = context.bloc<AddToCalendarCubit>();
-                calendarCubit.addToCalendar(_calendarEvent, '2');
-              },
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            );
-          } else {
-            return FloatingActionButton(
-              onPressed: () {
-                final calendarCubit = context.bloc<AddToCalendarCubit>();
-                calendarCubit.addToCalendar(_calendarEvent, '2');
-              },
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            );
+          if (state is AddToCalendarInitial) {
+            return _buildInitialLayout();
           }
+          if (state is AddToCalendarInProgress) {
+            return CircularProgressIndicator();
+          } else {
+            return _buildEventFormLayout();
+          }
+        },
+      ),
+      floatingActionButton:
+          BlocConsumer<AddToCalendarCubit, AddToCalendarState>(
+              listener: (context, state) {
+        if (state is AddToCalendarFailure) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(state.message),
+          ));
         }
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        if (state is AddToCalendarSuccess) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(state.message),
+          ));
+        }
+      }, builder: (context, state) {
+        if (state is AddToCalendarInitial) {
+          return FloatingActionButton(
+            onPressed: () {
+              final calendarCubit = context.bloc<AddToCalendarCubit>();
+              calendarCubit.addToCalendar(_calendarEvent, '2');
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        } else if (state is AddToCalendarInProgress) {
+          return CircularProgressIndicator();
+        } else if (state is AddToCalendarSuccess) {
+          return FloatingActionButton(
+            onPressed: () {
+              final calendarCubit = context.bloc<AddToCalendarCubit>();
+              calendarCubit.addToCalendar(_calendarEvent, '2');
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        } else {
+          return FloatingActionButton(
+            onPressed: () {
+              final calendarCubit = context.bloc<AddToCalendarCubit>();
+              calendarCubit.addToCalendar(_calendarEvent, '2');
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          );
+        }
+      }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -101,9 +102,49 @@ class _CalendarOverviewPageState extends State<CalendarOverviewPage> {
       eventDescription: eventDescription,
       eventDurationInHours: 3,
     );
+
+    final calendarCubit = context.bloc<AddToCalendarCubit>();
+    calendarCubit.addToCalendar(_calendarEvent, '0');
   }
 
   Widget _buildInitialLayout() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              'Event Calendar App',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 18.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                width: double.infinity,
+                child: RaisedButton(
+                    color: Colors.deepOrange,
+                    onPressed: () async {
+                      final calendarCubit = context.bloc<AddToCalendarCubit>();
+                      _calendars = await calendarCubit.loadCalendars();
+                    },
+                    child: Text(
+                      'Show Calendars',
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventFormLayout() {
+    final TextEditingController eventNameController = TextEditingController();
+    final TextEditingController eventDescriptionController =
+        TextEditingController();
+
     return Center(
       child: SingleChildScrollView(
         child: Column(
@@ -114,7 +155,62 @@ class _CalendarOverviewPageState extends State<CalendarOverviewPage> {
               style: Theme.of(context).textTheme.headline4,
             ),
             const SizedBox(height: 18.0),
-            EventFormWidget(_submitEvent),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: eventNameController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 5.0),
+                            ),
+                            labelText: 'Event Name'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: eventDescriptionController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 5.0),
+                            ),
+                            labelText: 'Event description'),
+                        maxLines: 3,
+                        maxLength: 250,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                          color: Colors.deepOrange,
+                          onPressed: () {
+                            if (eventNameController.text != null &&
+                                eventDescriptionController.text != null) {
+                              _submitEvent(eventNameController.text,
+                                  eventDescriptionController.text);
+                            } else {
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text('Information missing'),
+                              ));
+                            }
+                          },
+                          child: Text(
+                            'Create Event',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
